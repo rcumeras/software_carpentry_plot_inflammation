69b44c4e9e2667f2ee171a0e0e132bc9e0c907bb
Raquel Cumeras <raquel.cumeras@gmail.com>
2014-06-17 16:00:16 -0700

    2

--- plot_inflammation.r
+++ plot_inflammation.r
@@ -8,7 +8,7 @@
 # Load libraries
 require(ggplot2)
 require(reshape2)
-# Here's a comment
+# Here's a comment 2
 
 # Takes one command line argument as input data set
 options <- commandArgs(trailingOnly = TRUE)

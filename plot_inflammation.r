3c9683b2472f7d8290d19465d2f313768c6d4ae7
Raquel Cumeras <raquel.cumeras@gmail.com>
2014-06-17 11:56:33 -0700

    Added plot title

--- plot_inflammation.r
+++ plot_inflammation.r
@@ -22,7 +22,8 @@ data.in.melted <- melt(data.in)
 out.plot <- ggplot(data.in.melted,(aes(x=PatientID, y=value))) +
 geom_point() +
 stat_sum() +
-theme(axis.text.x = element_text(angle = 90, vjust = 0.5, hjust=1))
+theme(axis.text.x = element_text(angle = 90, vjust = 0.5, hjust=1))+
+ggtitle(paste("File: ",options[1]) #options[1] is the filename
 # save pdf based on input file name
 savename <- paste0(options[1],".pdf")
 ggsave(out.plot, file=savename, width = 12, height = 8)

da098f3c27921d137bc4c9b0ef552bcb1d191075
Raquel Cumeras <raquel.cumeras@gmail.com>
2014-06-17 15:50:03 -0700

    deleted a space

--- plot_inflammation.r
+++ plot_inflammation.r
@@ -8,7 +8,8 @@
 # Load libraries
 require(ggplot2)
 require(reshape2)
- # Here's a comment
+# Here's a comment
+
 # Takes one command line argument as input data set
 options <- commandArgs(trailingOnly = TRUE)
  
